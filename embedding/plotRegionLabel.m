87a6821fd9b6ae79244c3299bd6cfa19d5ae03ab
Shuting Han <shuting.han1@gmail.com>
2016-09-29 17:58:08 -0400

    corrected region label centers

--- embedding/plotRegionLabel.m
+++ embedding/plotRegionLabel.m
@@ -1,18 +1,24 @@
-function [] = plotRegionLabel(seg_im,im_mask,xx)
+function [] = plotRegionLabel(xx,seg_im,im_mask)
 % plot segmentation regions with labels
 
-imagesc(xx,xx,double(seg_im).*double(im_mask));
-colormap(gray);
+numPoints = length(xx);
+im = double(seg_im);
+im(im_mask==0) = NaN;
+im(im==0) = NaN;
+im(~isnan(im)) = 1;
+pcolor(xx,xx,im);
+colormap(gca,gray);
+shading flat
 axis equal tight off xy
 title('watershed segmentation');
 region_cent = regionprops(double(seg_im).*double(im_mask),'Centroid');
 for i = 1:length(region_cent)
-    tmpCoord = [region_cent(i).Centroid(1),region_cent(i).Centroid(2)];
-    tmpNum = seg_im(round(tmpCoord(2)),round(tmpCoord(1)));
-    if tmpNum~=0
-        h = text(double(2*maxVal*(tmpCoord(1)-numPoints/2)/numPoints),...
-            double(2*maxVal*(tmpCoord(2)-numPoints/2)/numPoints),num2str(tmpNum));
-        set(h,'color','y','fontsize',10);
+    regCoord = region_cent(i).Centroid;
+    regNum = seg_im(round(regCoord(2)),round(regCoord(1)));
+    if regNum~=0
+        h = text(2*regCoord(1)/numPoints*xx(end)-xx(end),...
+            2*regCoord(2)/numPoints*xx(end)-xx(end),num2str(regNum));
+        set(h,'color','y','fontsize',6);
     end
 end
 
